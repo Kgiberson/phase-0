1bb38349451f2500e7c5aa8c538dcebd87d5680b
Kgiberson <kari.giberson@gmail.com>
2016-02-20 13:59:56 -0800

    Add refleciton to 5.5

--- phase-0-reflections/week-5/nums-commas/my_solution.rb
+++ phase-0-reflections/week-5/nums-commas/my_solution.rb
@@ -70,11 +70,12 @@ end
 # 3. Reflection
 # Things I struggled with: How to separate a string of numbers into its individual digits.
 # What was your process for breaking the problem down? What different approaches did you consider?
-#
+  # While pseudocoding, I considered inserting the comma while iterating through the test_string but realized that I didn't yet have a strong understanding of strings as objects. I also looked into using the mod operator to divide through the integer instead of creating an array to store it.
 # Was your pseudocode effective in helping you build a successful initial solution?
-#
+  # Yes and no, I actually found it more helpful to talk the process aloud because the words on my screen were not connecting fully with my brain.
 # What new Ruby method(s) did you use when refactoring your solution? Describe your experience of using the Ruby documentation to implement it/them (any difficulties, etc.). Did it/they significantly change the way your code works? If so, how?
-#
+  # I found .each_char by reading through the methods listed in the string ruby docs. I also found it helpful to review the % operator and it's funcitoning by referencing the ruby docs. The Ruby docs for Newbies resource was helpful in introducing best practices for how/when to search through the docs and find relevant methods. I found that .map could be a good alternative to .each but decided to stick with .each.
 # How did you initially iterate through the data structure?
-#
+  # I initally used .each_char to iterate through the string and turn it into an array, and used .each to iterate through the test_arr to form the final_arr
 # Do you feel your refactored solution is more readable than your initial solution? Why?
+  # Yes, I was able to remove a redundant if statement.
