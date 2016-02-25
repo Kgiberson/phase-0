6926683c58eb7f9ca0244f537ffe595c275445a7
Kgiberson <kari.giberson@gmail.com>
2016-02-25 14:25:34 -0800

    Add 6.7 file

---
+++ phase-0-reflections/week-6/guessing-game/my_solution.rb
@@ -0,0 +1,43 @@
+
+# Build a simple guessing game
+
+
+# I worked on this challenge by myself.
+# I spent [#] hours on this challenge.
+
+# Pseudocode
+
+# Input:
+# Output:
+# Steps:
+1. Initialize class with an integer
+2. Define an instance method which takes an integer (guess) as its input.
+  - guess returns :high IF guess > answer
+  - guess returns :correct IF guess = answer
+  - guess returns :low IF guess < answer
+3. Define an instance method (GuessingGame.solved?)
+  - returns true IF most recent guess = correct
+  - ELSE returns false
+
+
+# Initial Solution
+
+class GuessingGame
+  def initialize(answer)
+    # Your initialization code goes here
+  end
+
+  # Make sure you define the other required methods, too
+end
+
+
+
+
+# Refactored Solution
+
+
+
+
+
+
+# Reflection
