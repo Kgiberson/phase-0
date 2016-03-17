867b7146c87c77e9d955476b362827a8d8e44cea
Kgiberson <kari.giberson@gmail.com>
2016-03-17 05:44:44 -0700

    Add reflection to 9.3.1 file

--- phase-0-reflections/week-9/dom-manipulation/home_page.js
+++ phase-0-reflections/week-9/dom-manipulation/home_page.js
@@ -9,14 +9,14 @@
 // Release 0:
 
 
-
+// window.onload = function(){
 // Release 1:
 var release0 = document.getElementById("release-0"); release0.setAttribute("class","done");
 
 
 
 // Release 2:
-var release1 = document.getElementsById("release-1").style.display = "none";
+var release1 = document.getElementById("release-1").style.display = "none";
 
 
 
@@ -38,3 +38,10 @@ var release4 = document.getElementsByClassName("release-4")[0,1].style.fontSize 
 // Release 6:
 var template = document.getElementById("hidden");
 document.body.appendChild(template.content.cloneNode(true));
+// }
+
+// Reflect:
+// What did you learn about the DOM?
+  // I learned that when an HTML document is loaded into a web browser, it becomes a document object, and is the root node of the HTML document. The document object provides properties and methods to access all node objects from within JavaScript. the DOM is a programming interface for HTML and XML documents. The DOM provides a representation of the document as a structured group of nodes and objects that have properties and methods. It connects web pages to scripts or programming languages.
+// What are some useful methods to use to manipulate the DOM?
+  // getElementById and getElementsByTagName were useful for selecting the right elements, and I'm excited to play around with the methods associated with the template element.
